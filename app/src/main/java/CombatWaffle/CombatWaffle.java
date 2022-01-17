45342c1c51e8b49a710f7823086abe1502dd3f1a
TheNewTimeGamer <32019721+TheNewTimeGamer@users.noreply.github.com>
2022-01-17 04:08:48 +0100

    Old CombatWaffle is now known as 'optic' while new feature is being implemented.

--- app/src/main/java/CombatWaffle/CombatWaffle.java
+++ app/src/main/java/CombatWaffle/CombatWaffle.java
@@ -1,155 +1,79 @@
 package CombatWaffle;
 
-import java.awt.AWTException;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import javax.swing.JFrame;
-import java.awt.Robot;
-import java.awt.Rectangle;
-import java.awt.image.BufferStrategy;
-import java.awt.Canvas;
+import javax.swing.JPanel;
+
+import java.io.File;
+import java.io.IOException;
+
 import java.awt.Graphics;
-import java.awt.MouseInfo;
-import java.awt.image.BufferedImage;
-import java.awt.event.*;
-import java.awt.Point;
-import java.awt.PointerInfo;
-import java.awt.Toolkit;
 import java.awt.Color;
-import java.awt.geom.Ellipse2D;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-public class CombatWaffle extends Canvas implements Runnable {
+import java.awt.Dimension;
+
+public class CombatWaffle {
     
-    Dimension screenDimensions;
-    JFrame frame;
+    private final JFrame backgroundFrame;
+    private final WeaponPanel weaponPanel;
 
-    Thread captureThread = new Thread(this);
-
-    double magnification = 1.0;
-
-    Point globalMousePosition = new Point(0,0);
-    Point localMousePosition = new Point(0,0);
-
-    boolean lockedLocalMousePosition = false;
+    private BufferedImage weaponImage = null;
 
     public CombatWaffle() {
-        this.screenDimensions = Toolkit.getDefaultToolkit().getScreenSize();
-        this.frame = new JFrame();
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setSize(this.screenDimensions.width/40, this.screenDimensions.width/40);
-        this.frame.setLocationRelativeTo(null);
-        this.frame.add(this);
-        this.frame.setFocusable(false);
-        this.frame.setAlwaysOnTop(true);
-        this.frame.setUndecorated(true);
-        this.frame.setShape(new Ellipse2D.Double(0,0,this.frame.getWidth(),this.frame.getHeight()));
-        this.frame.setVisible(true);
+        this.weaponPanel = new WeaponPanel(this);
+        this.weaponPanel.setBackground(new Color(0,0,0,0));
 
-        this.addKeyListener(new KeyAdapter(){
-            public void keyReleased(KeyEvent e){
-                if(e.getKeyCode() == KeyEvent.VK_F3) {
-                    System.exit(0);
-                }
-            }
-        });
+        this.backgroundFrame = new JFrame();
+        this.backgroundFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.backgroundFrame.setUndecorated(true);
+        this.backgroundFrame.setSize(1020,964);
 
-        this.addMouseListener(new MouseAdapter(){
-            public void mousePressed(MouseEvent e) {
-                lockedLocalMousePosition = true;
-            }
-            public void mouseReleased(MouseEvent e) {
-                lockedLocalMousePosition = false;
-            }
-        });
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int frameX = (screenSize.width-this.backgroundFrame.getWidth())/2 - 64;
+        int frameY = screenSize.height-this.backgroundFrame.getHeight() + 275;
 
-        this.addMouseMotionListener(new MouseMotionListener() {
-            public void mouseDragged(MouseEvent e) {
-                frame.setLocation(globalMousePosition.x - localMousePosition.x, globalMousePosition.y - localMousePosition.y);
-            }
-
-            public void mouseMoved(MouseEvent e) {
-                
-            }
-        });
-
-        this.addMouseWheelListener(new MouseWheelListener() {
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if(e.getWheelRotation() > 0){
-                    magnification -= 0.5;
-                    if(magnification < 1) {
-                        magnification = 1;
-                    }
-                }else if(e.getWheelRotation() < 0){
-                    magnification += 0.5;
-                }
-            }
-        });
-
-        this.captureThread.start();
+        this.backgroundFrame.setLocation(frameX, frameY);
+        this.backgroundFrame.setBackground(new Color(0,0,0,0));
+        this.backgroundFrame.add(this.weaponPanel);
+        //this.backgroundFrame.setFocusable(false);
+        this.backgroundFrame.setAlwaysOnTop(true);
+        this.backgroundFrame.setVisible(true);
     }
 
-    public void run() {
-        long last = 0;
-        while(true){
-            Point p = null;
+    public BufferedImage getWeaponImage() {
+        if(this.weaponImage == null){
             try{
-                p = this.getMousePosition();
-            }catch(Exception e){}
-            if(p != null){
-                if(!lockedLocalMousePosition) {
-                    this.localMousePosition = p;
-                }
-            }
-            this.globalMousePosition = MouseInfo.getPointerInfo().getLocation();
-            if(System.currentTimeMillis()-last > (1000/60)){
-                render();
-            }
-        }
-    }
-
-    Robot robot;
-
-    int screenCaptureModifier = 5;
-
-    private BufferedImage capture() { 
-        if(this.robot == null){
-            try{
-                this.robot = new Robot();
-            }catch(AWTException e){
+                this.weaponImage = ImageIO.read(new File("weapon.png"));
+            }catch(IOException e){
                 e.printStackTrace();
-                return null;
             }
         }
-
-        int xOffset = screenDimensions.width / 2 - (screenDimensions.width / screenCaptureModifier / 2);
-        int yOffset = screenDimensions.height / 2 - (screenDimensions.width / screenCaptureModifier / 2);
-        return this.robot.createScreenCapture(new Rectangle(xOffset, yOffset, screenDimensions.width/screenCaptureModifier, screenDimensions.width/screenCaptureModifier));
+        return this.weaponImage;
     }
 
-    private void render() {
-        BufferStrategy bs = this.getBufferStrategy();
-        if(bs == null) {
-            this.createBufferStrategy(3);
-            return;
+}
+
+class WeaponPanel extends JPanel {
+
+    public final CombatWaffle instance;
+
+    public WeaponPanel(CombatWaffle instance){
+        this.instance = instance;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        BufferedImage weaponImage = this.instance.getWeaponImage();
+        if(weaponImage != null){
+            g.drawImage(weaponImage, 0, 0, this.getWidth(), this.getHeight(), null);
+        }else{
+            g.setColor(Color.RED);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
         }
-
-        BufferedImage screen = capture();
-
-        Graphics g = bs.getDrawGraphics();
-        if(screen != null){
-            int xOffset = (int) - this.magnification;
-            int yOffset = (int) - this.magnification;
-            int width = (int) (this.getWidth() + this.magnification * 2);
-            int height = (int) (this.getHeight() + this.magnification * 2);
-            g.drawImage(screen, xOffset, yOffset, width, height, null);
-        }
-
-        g.setColor(Color.RED);
-        g.drawOval(0, 0, this.getWidth()-1, this.getHeight()-1);
-        g.drawOval(1, 1, this.getWidth()-3, this.getHeight()-3);
-
-        g.dispose();
-        bs.show();
     }
 
 }
